c2686c93e4d97f270d4fc7f7a68873ace3e7cfe1
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-04-08 18:18:54 -0300

    metodos update e delete de projects corrgigidos. Teste de repositório 'like' reprovando

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
@@ -25,6 +25,8 @@ app.post("/repositories", (request, response) => {
 
   repositories.push(repository);
 
+
+
   return response.json(repository);
 });
 
@@ -32,15 +34,23 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+     
 
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+   
+  
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+ 
+
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
   repositories[repositoryIndex] = repository;
+
+  
 
   return response.json(repository);
 });
@@ -50,7 +60,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -70,7 +80,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(likes);
 });
 
 module.exports = app;
